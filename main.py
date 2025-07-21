e4e5a22cfab11efb64922d9b458e484b5c1a7f8c
曾斌 <zengbin@xiaohongshu.com>
2025-07-21 20:44:23 +0800

    feat: 初始化

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.all import AstrBotConfig
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context,config: AstrBotConfig):
         super().__init__(context)
+        self.context = context
+        self.config = config
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
